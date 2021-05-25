a75c74f129ae3a6ea126fed621af2e690cd01b74
ISheep <810230854@qq.com>
2021-05-25 18:35:10 +0800

    feat(main): 编码问题，数组拼接

--- src/main/java/http/Server.java
+++ src/main/java/http/Server.java
@@ -1,11 +1,7 @@
 package http;
 
-import java.io.UnsupportedEncodingException;
-import java.text.SimpleDateFormat;
+
 import java.util.Arrays;
-import java.util.Date;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * @author 林子洋靓
@@ -15,6 +11,39 @@ import java.util.concurrent.Executors;
  */
 public class Server {
 
-
+    public static void main(String[] args) {
+        int i, j = 0;
+        byte[] bytes = new byte[]{1, 2, 3, 5, 3, -2, -4, -6, -1, -9, 2, 4, 2, -1, -2, -3, 3, 4, 6};
+        byte[] newByte = new byte[1024];
+        for (i = 0; i < bytes.length; i++) {
+            if (bytes[i] < 0) {
+                // 英文
+                byte[] a1 = Arrays.copyOfRange(bytes, j, i);
+                j = i;
+                // ArrayUtil.addAll(newByte, a1);
+                System.arraycopy(a1, 0, newByte, 0, a1.length);
+                System.err.println(Arrays.toString(a1));
+                for (int k = i; k < bytes.length; k++) {
+                    if (bytes[k] > 0) {
+                        // 中文
+                        byte[] a2 = Arrays.copyOfRange(bytes, j, k);
+                        System.arraycopy(a2, 0, newByte, 0, a2.length);
+                        System.out.println(Arrays.toString(a2));
+                        // ArrayUtil.addAll(newByte, a2);
+                        j = k;
+                        i = k;
+                        break;
+                    }
+                }
+            } else if (i == bytes.length - 1) {
+                // 最后一段英文
+                byte[] last = Arrays.copyOfRange(bytes, j, i + 1);
+                System.err.println(Arrays.toString(last));
+                // ArrayUtil.addAll(newByte, last);
+            }
+        }
+        System.out.println("----------------------");
+        System.out.println(Arrays.toString(newByte));
+    }
 
 }
---
+++ src/test/java/NumberTest.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 林子洋靓
+ * @create 2021-05-21 15:30:33
+ */
+public class NumberTest {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add(null);
+        list.add(null);
+        if (list == null) {
+            System.out.println("222");
+        }
+        System.out.println(list);
+    }
+
+}
